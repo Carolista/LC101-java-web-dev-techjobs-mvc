ee82195a39882676a5ab9ed971350bf3e930011a
Carolista <55961845+Carolista@users.noreply.github.com>
2020-01-12 00:33:56 -0600

    super bonus complete

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -15,7 +15,11 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
  */
 @Controller
 @RequestMapping("search")
-public class SearchController {
+public class SearchController extends TechJobsController {
+
+    public SearchController() {
+        super();
+    }
 
     @RequestMapping(value = "")
     public String search(Model model) {
